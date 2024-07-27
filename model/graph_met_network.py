fixed scaling

model/graph_met_network.py
@@ -9,9 +9,12 @@ from torch_geometric.nn.conv import GraphConv, EdgeConv, GCNConv
 from torch_cluster import radius_graph, knn_graph
 
 class GraphMETNetwork(nn.Module):
-    def __init__ (self, continuous_dim, cat_dim, output_dim=1, hidden_dim=32, conv_depth=1):
+    def __init__ (self, continuous_dim, cat_dim, norm, output_dim=1, hidden_dim=32, conv_depth=1):
+    # def __init__ (self, continuous_dim, cat_dim, output_dim=1, hidden_dim=32, conv_depth=1):    
         super(GraphMETNetwork, self).__init__()
         
+        self.datanorm = norm
+
         self.embed_charge = nn.Embedding(3, hidden_dim//4)
         self.embed_pdgid = nn.Embedding(7, hidden_dim//4)
         self.embed_pv = nn.Embedding(8, hidden_dim//4)
@@ -48,6 +51,10 @@ class GraphMETNetwork(nn.Module):
         print("x_cont:",x_cont)
         print("x_cat:",x_cat)
         print("edge_index:",edge_index)
+        
+        #scale momentum
+        
+        x_cont *= self.datanorm
         #print("cont")
         emb_cont = self.embed_continuous(x_cont)   
 

train.py
@@ -82,9 +82,9 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(device)
 
-    norm = torch.tensor([1./scale_momentum, 1./scale_momentum, 1./scale_momentum, 1., 1., 1.]).to(device)   # pt, px, py: scale by 128      
+    norm = torch.tensor([1./scale_momentum, 1./scale_momentum, 1./scale_momentum, 1., 1., 1., 1., 1.]).to(device)   # pt, px, py: scale by 128      
     
-    model = net.Net(8, 3).to(device) #include puppi
+    model = net.Net(8, 3, norm).to(device) #include puppi
     print('model initialized')
     #model = net.Net(7, 3).to(device) #remove puppi
     optimizer = torch.optim.AdamW(model.parameters(),lr=0.001, weight_decay=0.001)